15f84f0abf464623aaa6a80a0adebfaf9cb9637e
Ross <rmojgani99@gmail.com>
2020-01-11 16:34:11 -0800

    run public ip

--- backend/flask_server.py
+++ backend/flask_server.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 
+"""
+Flask API which serves the frontend of the application
+and has different functionalities based on different
+requests
+
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
+"""
+Stores users into database with correct fields
+
+"""
+@app.route("/rapidserve/api/v1.0/users", methods=['POST'])
+def store_users():
     return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0:PORT#")
